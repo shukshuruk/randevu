da6afb01057837ee6ac4615f7fb7a6d62496416c
mkabanova <maria.k@inventain.com>
2016-07-13 15:33:35 +0300

    Add new play btn, background changed

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+      { test: /\.svg$/, loader: 'babel!svg-react' }
     ]
   }
 }
